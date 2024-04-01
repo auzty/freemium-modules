30192e1e8ac90bb41a7ae0304681b6d20af68176
yusuf <thea.uzty@gmail.com>
2024-04-01 09:08:37 +0700

    Testing Finalize with proper varname

--- index.js
+++ index.js
@@ -2,31 +2,20 @@ const Queue = require('bull');
 
 let  redisconf = {}
 
-exports.setRedisConfig = function(host,port,password){
-    const newhost = host || "127.0.0.1"
-    const newport = port || 6379
-    const newpassword = password || ""
-
-    redisconf = {
-        redis: {
-            host: newhost,
-            port: newport,
-            password: newpassword
-        }
-    }
+exports.setRedisConfig = function(redisconfig){
+   redisconf={redis: redisconfig}
 }
 
 exports.printMsg = function() {
   console.log("This is a message from the demo package");
 }
 
-exports.sendQueue = async function(inputku,queuename){
+exports.sendQueue = async function(data,qName){
     try{
-        qname = queuename || "default"
-        const myFirstQueue = new Queue(qname,redisconf);
-        const mydata = inputku || "default"
-        await myFirstQueue.add(mydata)
-        console.log("this is inputku : ",mydata)
+        qName = qName || "default"
+        const qConn = new Queue(qName,redisconf);
+        const inputData = data || "Default Input"
+        await qConn.add(inputData)
     }catch(e){
         throw new Error(e.message)
     }
